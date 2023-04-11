f73aba841616cd44c8af6b5093eaeb9694c4d96f
Lukas <macku.lukas@gmail.com>
2023-04-11 21:27:29 +0200

    Backend is done

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -19,9 +19,11 @@ const getAllTasks = async (req, res) => {
 
 const createTask = async (req, res) => {
   const { title, section, date, time } = req.body;
+  // Check if body has all needed things
   if (!title || !section || !date || !time) {
     return res.status(400).json({ error: 'All fields are required!' });
   }
+  // Creating task
   const createdTask = await Todo.create({ title, section, date, time });
   res.status(201).json(createdTask);
 };
@@ -33,10 +35,12 @@ const createTask = async (req, res) => {
  */
 
 const updateTask = async (req, res) => {
+  // Check if task exists
   const task = await Todo.findById(req.params.id);
   if (!task) {
     return res.status(404).json({ error: 'Task not found' });
   }
+  // Updating task
   const updatedTask = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.status(200).json(updatedTask);
 };
@@ -48,10 +52,12 @@ const updateTask = async (req, res) => {
  */
 
 const deleteTask = async (req, res) => {
+  // Check if task exists
   const task = await Todo.findById(req.params.id);
   if (!task) {
     return res.status(404).json({ error: 'Task not found' });
   }
+  // Deleting task
   const taskToDel = await Todo.findByIdAndDelete(req.params.id);
   res.status(202).json(taskToDel);
 };
--- server/main.js
+++ server/main.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const todoRouter = require('./routes/todoRoute');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectDB();
 
 app.use('/api', todoRouter);
