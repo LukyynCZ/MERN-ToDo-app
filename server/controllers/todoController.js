4c2cf5e4ca0b5663fa42f6414e0afd9f09692c0f
Lukas <macku.lukas@gmail.com>
2023-04-15 03:38:57 +0200

    Final App

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -7,8 +7,12 @@ const Todo = require('../models/todoModel');
  */
 
 const getAllTasks = async (req, res) => {
-  const allTasks = await Todo.find();
-  res.status(200).json(allTasks);
+  try {
+    const allTasks = await Todo.find();
+    res.status(200).json(allTasks);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -24,8 +28,12 @@ const createTask = async (req, res) => {
     return res.status(400).json({ error: 'All fields are required!' });
   }
   // Creating task
-  const createdTask = await Todo.create({ title, section, date, time });
-  res.status(201).json(createdTask);
+  try {
+    const createdTask = await Todo.create({ title, section, date, time });
+    res.status(201).json(createdTask);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -37,12 +45,17 @@ const createTask = async (req, res) => {
 const updateTask = async (req, res) => {
   // Check if task exists
   const task = await Todo.findById(req.params.id);
+
   if (!task) {
     return res.status(404).json({ error: 'Task not found' });
   }
   // Updating task
-  const updatedTask = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.status(200).json(updatedTask);
+  try {
+    const updatedTask = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -54,12 +67,17 @@ const updateTask = async (req, res) => {
 const deleteTask = async (req, res) => {
   // Check if task exists
   const task = await Todo.findById(req.params.id);
+
   if (!task) {
     return res.status(404).json({ error: 'Task not found' });
   }
   // Deleting task
-  const taskToDel = await Todo.findByIdAndDelete(req.params.id);
-  res.status(202).json(taskToDel);
+  try {
+    const taskToDel = await Todo.findByIdAndDelete(req.params.id);
+    res.status(202).json(taskToDel);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { getAllTasks, createTask, updateTask, deleteTask };
